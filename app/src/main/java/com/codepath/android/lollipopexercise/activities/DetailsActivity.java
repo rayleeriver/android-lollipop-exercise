7313a5716c414ef3944d0eb95f7274e28ae450cc
Raymond Lee <rayleeriver@gmail.com>
2015-04-01 21:33:53 -0700

    init

--- app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/activities/DetailsActivity.java
@@ -1,7 +1,10 @@
 package com.codepath.android.lollipopexercise.activities;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.graphics.Palette;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,6 +13,7 @@ import android.widget.TextView;
 import com.codepath.android.lollipopexercise.R;
 import com.codepath.android.lollipopexercise.models.Contact;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 public class DetailsActivity extends ActionBarActivity {
     public static final String EXTRA_CONTACT = "EXTRA_CONTACT";
@@ -33,7 +37,42 @@ public class DetailsActivity extends ActionBarActivity {
         mContact = (Contact)getIntent().getExtras().getSerializable(EXTRA_CONTACT);
 
         // Fill views with data
-        Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
+//        Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
+
+        Target target = new Target() {
+
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(ivProfile);
+
+
+                Palette.generateAsync(bitmap, new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        Palette.Swatch vibrant = palette.getVibrantSwatch();
+                        if (vibrant != null) {
+                            vPalette.setBackgroundColor(vibrant.getRgb());
+                            tvName.setTextColor(vibrant.getTitleTextColor());
+                        }
+                    }
+                });
+
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        ivProfile.setTag(target);
+        Picasso.with(DetailsActivity.this).load(mContact.getThumbnailDrawable()).into(target);
+
         tvName.setText(mContact.getName());
         tvPhone.setText(mContact.getNumber());
     }
--- app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
+++ app/src/main/java/com/codepath/android/lollipopexercise/adapters/ContactsAdapter.java
@@ -2,16 +2,25 @@ package com.codepath.android.lollipopexercise.adapters;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.codepath.android.lollipopexercise.R;
+import com.codepath.android.lollipopexercise.activities.ContactsActivity;
+import com.codepath.android.lollipopexercise.activities.DetailsActivity;
 import com.codepath.android.lollipopexercise.models.Contact;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.List;
 
@@ -37,11 +46,48 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
 
     // Display data at the specified position
     @Override
-    public void onBindViewHolder(VH holder, int position) {
-        Contact contact = mContacts.get(position);
+    public void onBindViewHolder(final VH holder, int position) {
+        final Contact contact = mContacts.get(position);
         holder.rootView.setTag(contact);
         holder.tvName.setText(contact.getName());
-        Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(holder.ivProfile);
+
+
+//        Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(holder.ivProfile);
+
+        Target target = new Target() {
+
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(holder.ivProfile);
+
+
+                Palette.generateAsync(bitmap, new Palette.PaletteAsyncListener() {
+                    @Override
+                    public void onGenerated(Palette palette) {
+                        Palette.Swatch vibrant = palette.getVibrantSwatch();
+                        if (vibrant != null) {
+                            holder.vPalette.setBackgroundColor(vibrant.getRgb());
+                            holder.tvName.setTextColor(vibrant.getTitleTextColor());
+                        }
+                    }
+                });
+
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+
+        holder.ivProfile.setTag(target);
+        Picasso.with(mContext).load(contact.getThumbnailDrawable()).into(target);
+
     }
 
     @Override
@@ -59,18 +105,25 @@ public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.VH> {
         public VH(View itemView, final Context context) {
             super(itemView);
             rootView = itemView;
-            ivProfile = (ImageView)itemView.findViewById(R.id.ivProfile);
-            tvName = (TextView)itemView.findViewById(R.id.tvName);
+            ivProfile = (ImageView) itemView.findViewById(R.id.ivProfile);
+            tvName = (TextView) itemView.findViewById(R.id.tvName);
             vPalette = itemView.findViewById(R.id.vPalette);
 
             // Navigate to contact details activity on click of card view.
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    final Contact contact = (Contact)v.getTag();
+                    final Contact contact = (Contact) v.getTag();
                     if (contact != null) {
                         // Fire an intent when a contact is selected
                         // Pass contact object in the bundle and populate details activity.
+
+                        Intent i = new Intent(context, DetailsActivity.class);
+                        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((ContactsActivity) context, (View) ivProfile, "profile");
+
+                        i.putExtra(DetailsActivity.EXTRA_CONTACT, contact);
+                        context.startActivity(i, options.toBundle());
+
                     }
                 }
             });
